32ef98185a47167a9217e8f18dfe5108a2972e9f
Cuong Dinh Lai <laidinhcuongvn@gmail.com>
2014-12-12 22:27:39 +0700

    Update SB Theme version 1.2.3

--- inc/class-sb-detect.php
+++ inc/class-sb-detect.php
@@ -22,6 +22,67 @@ class SB_Detect {
         return SB_Core::get_visitor_ip();
     }
 
+    public static function get_visitor_operating_system() {
+        return SB_PHP::get_operating_system();
+    }
+
+    public static function get_visitor_operating_sytem_code() {
+        $os = self::get_visitor_operating_system();
+        $os = SB_PHP::lowercase($os);
+        $result = '';
+        if(self::is_mobile()) {
+            if(!class_exists('Mobile_Detect')) {
+                require SB_THEME_LIB_PATH . '/mobile-detect/Mobile_Detect.php';
+            }
+            $detect = new Mobile_Detect();
+            if($detect->is('AndroidOS')) {
+                $result = 'android';
+            } elseif($detect->is('BlackBerryOS')) {
+                $result = 'blackberry';
+            } elseif($detect->is('PalmOS')) {
+                $result = 'palm';
+            } elseif($detect->is('SymbianOS')) {
+                $result = 'symbian';
+            } elseif($detect->is('WindowsMobileOS')) {
+                $result = 'windowsmobile';
+            } elseif($detect->is('WindowsPhoneOS')) {
+                $result = 'windowsphone';
+            } elseif($detect->is('iOS')) {
+                $result = 'ios';
+            } elseif($detect->is('MeeGoOS')) {
+                $result = 'meego';
+            } elseif($detect->is('MaemoOS')) {
+                $result = 'maemo';
+            } elseif($detect->is('JavaOS')) {
+                $result = 'java';
+            } elseif($detect->is('webOS')) {
+                $result = 'web';
+            } elseif($detect->is('badaOS')) {
+                $result = 'bada';
+            } elseif($detect->is('BREWOS')) {
+                $result = 'brew';
+            }
+        } else {
+            if(SB_PHP::is_string_contain($os, 'windows')) {
+                $result = 'windows';
+            } elseif(SB_PHP::is_string_contain($os, 'linux')) {
+                $result = 'linux';
+            } elseif(SB_PHP::is_string_contain($os, 'mac')) {
+                $result = 'mac';
+            }
+        }
+        return $result;
+    }
+
+    public static function get_visitor_country_code_by_ip($ip_address) {
+        return SB_PHP::get_country_code_by_ip($ip_address);
+    }
+
+    public static function get_visitor_country_code() {
+        $ip_address = self::get_visitor_ip();
+        return self::get_visitor_country_code_by_ip($ip_address);
+    }
+
     public static function get_bots() {
         $result = '';
         $bots_list = array(
